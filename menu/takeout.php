41857fb14cec412f09c8f9633658b6681f7eac55
nahee-park <jessica396@ewhain.net>
2022-06-09 17:20:45 +0900

    feat: 이대학생/ 타대학생 주문 건수, 테이크아웃 건수

--- menu/takeout.php
+++ menu/takeout.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CORNER - 이대 학생/ 타대 학생 코너별 비율</title>
+    <title>Customer - 나이대별 주문건수</title>
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
@@ -18,35 +18,36 @@
         echo "Could not connect: " . mysqli_connect_error();
         exit();
     }
-    $query = "
-    SELECT CUSTOMER.IS_EWHAIN AS 이대학생여부, COUNT(ORDER_LIST.ORDER_NUM) AS 주문건수
-    FROM CUSTOMER , ORDER_LIST
-    WHERE CUSTOMER.PHONENUMBER = ORDER_LIST.CUSTOMER_PHONENUMBER
-    GROUP BY CUSTOMER.IS_EWHAIN WITH ROLLUP;
-    ";
+    $query = "SELECT DISTINCT MENU.MENU_NAME AS 메뉴이름, ORDER_LIST.IS_TAKEOUT AS 포장여부, COUNT(ORDER_LIST.ORDER_NUM ) AS 주문건수
+    FROM MENU, ORDER_LIST, MENU_ORDER
+    WHERE ORDER_LIST.ORDER_NUM = MENU_ORDER.ORDER_NUM AND
+    MENU_ORDER.MENU_ID = MENU.MENU_ID
+    GROUP BY MENU.MENU_NAME,ORDER_LIST.IS_TAKEOUT WITH ROLLUP;";
     $result = mysqli_query($conn,$query);
     ?>
     
     <FORM>
-    <h1>이대 학생/ 타대 학생 주문 건수</h1>
+    <h1>메뉴별 테이크아웃 건수</h1>
 
     <TABLE BORDER=1>
         <TR>
-            <TD>이대학생여부</TD>
-            <TD>주문건수</TD>
+            <TD>메뉴이름</TD>
+            <TD>포장여부</TD>
+            <td>주문건수</td>
         </TR>
         <?php 
         while($row = mysqli_fetch_array($result)){
             ?>
             <TR>
-                <TD><?=$row['이대학생여부']?></TD>
+                <TD><?=$row['메뉴이름']?></TD>
+                <TD><?=$row['포장여부']?></TD>
                 <TD><?=$row['주문건수']?></TD>
             </TR>
             <?php } ?>
         
         
 </TABLE>
-<p>*주문건수 기준 내림차순 정렬</p>
+<!-- <p>*주문빈도 기준 내림차순 정렬</p> -->
     </FORM>
     <?php 
     mysqli_free_result($result);
